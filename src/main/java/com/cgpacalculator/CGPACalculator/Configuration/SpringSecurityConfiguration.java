7143cfece228d42a81aa5ff6ca58741d6872b4c1
M.R.Nikhil <60879899+nikhilmr2000@users.noreply.github.com>
2022-12-20 22:38:55 +0530

    Update SpringSecurityConfiguration.java

--- src/main/java/com/cgpacalculator/CGPACalculator/Configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/cgpacalculator/CGPACalculator/Configuration/SpringSecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SpringSecurityConfiguration {
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 				
 				CorsConfiguration config=new CorsConfiguration();
-				config.setAllowedOrigins(Collections.singletonList("https://statuesque-narwhal-56e808.netlify.app/"));
+				config.setAllowedOrigins(Collections.singletonList("https://cgpacalculator-1g1ehs63l-nikhilmr2000.vercel.app/"));
 				config.addAllowedHeader("Access-Control-Allow-Origin");
 				config.addAllowedHeader("Content-Type");
 				config.setAllowedHeaders(Collections.singletonList("*"));
